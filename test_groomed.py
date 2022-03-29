4864fdfa686dbf4f9db3f4d5f8e3ae1e94e6d414
scottschmidl <sjschmidl@gmail.com>
2022-03-29 09:22:03 -0500

    added file and class for unit testing

---
+++ test_groomed.py
@@ -0,0 +1,30 @@
+from issue1_03282022_vector_calc.vector_calc import VectorCalculus
+from cryptography import Cryptography
+from differential_equations import DifferentialEquations
+from discrete_math import DiscreteMath
+from linear_algebra import LinearAlgebra
+from probability import Probability
+import unittest
+
+class TestGroomed(unittest.TestCase):
+
+    def test_vector_calc(self):
+        pass
+
+    def test_cryptography(self):
+        pass
+
+    def test_differential_equations(self):
+        pass
+
+    def test_disceret_math(self):
+        pass
+
+    def test_linear_algebra(self):
+        pass
+
+    def test_probability(self):
+        pass
+
+    if __name__ == "__main__":
+        tg = TestGroomed()
